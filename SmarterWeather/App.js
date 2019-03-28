822b2d16edebf6ba5fc4d5d5e0439f83ddd22afe
alexisd7 <45074741+alexisd7@users.noreply.github.com>
2019-03-27 21:58:34 -0400

    Lab 4

--- SmarterWeather/App.js
+++ SmarterWeather/App.js
@@ -17,33 +17,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date().toLocaleString()
-    };
-  }
-  componentDidMount() {
-    this.intervalID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
-  tick() {
-    this.setState({
-      time: new Date().toLocaleString()
-    });
-  }
-  render() {
-    return (
-      <p className="App-clock">
-        The time is {this.state.time}.
-      </p>
-    );
-  }
-}
